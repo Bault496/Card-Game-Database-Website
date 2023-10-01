05dcde3bcffeeb8f9706b9221b328592f64897c4
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-10-01 16:42:26 -0700

    Add project files.

---
+++ Card-Game-Database-Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Card_Game_Database_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

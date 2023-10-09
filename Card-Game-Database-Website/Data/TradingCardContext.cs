7d34eb30a2394e8f66487e2515df59512f1a7bf9
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-10-08 21:30:39 -0700

    Added TradingCardContext for Database

---
+++ Card-Game-Database-Website/Data/TradingCardContext.cs
@@ -0,0 +1,14 @@
+﻿using Card_Game_Database_Website.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Card_Game_Database_Website.Data
+{
+    public class TradingCardContext : DbContext
+    {
+        public TradingCardContext(DbContextOptions<TradingCardContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}

173f3001a1c70d889e15a62995c073810b93471b
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-10-08 23:02:36 -0700

    Got create functioning with the database

--- Card-Game-Database-Website/Controllers/CardsController.cs
+++ Card-Game-Database-Website/Controllers/CardsController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Card_Game_Database_Website.Data;
+using Card_Game_Database_Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IIS.Core;
 
 namespace Card_Game_Database_Website.Controllers
 {
     public class CardsController : Controller
     {
+        private readonly TradingCardContext _context;
+        public CardsController(TradingCardContext context)
+        {
+            _context = context;
+        }
         public IActionResult CardCreate()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CardCreate(Card TradingCard)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Cards.Add(TradingCard); //prepares insert
+                _context.SaveChanges(); // executes pending insert
+                // show sucess message
+                ViewData["Message"] = $"{TradingCard.CardName} was added successfully!";
+               
+                return View();
+            }
+
+            return View(TradingCard);
         }
     }
 }
